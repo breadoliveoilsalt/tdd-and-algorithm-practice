55b9f7265f7fa5aa00933216afb166d243bcbbce
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-01 15:48:23 -0400

    Clean up chai assertions at top of test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 var chai = require('chai')
 var assert = require('chai').assert
 var expect = chai.expect
-var should = chai.should();
+var should = chai.should()
 
 
 // REMEMBER TO REQUIRE OTHER FILES, EG - let firstTest = require("./myFirstTests.js")
