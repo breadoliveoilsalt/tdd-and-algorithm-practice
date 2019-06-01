263ce2ce4af0c255fe34a6d170d7af7316627d92
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-01 16:00:38 -0400

    In test.js, define simple function and draft mocha test for it. Passes.

--- test/test.js
+++ test/test.js
@@ -7,3 +7,17 @@ var should = chai.should()
 
 
 // REMEMBER TO REQUIRE OTHER FILES, EG - let firstTest = require("./myFirstTests.js")
+function addFiveTo(num) {
+  return 5 + num
+}
+
+describe("Test", function(){
+  describe("Testing #addFiveTo()", function(){
+    it("should return a number equal to five plus the argument passed to the function",
+    function(){
+      assert.equal(addFiveTo(7), 12)
+      assert.equal(addFiveTo(0), 5)
+      assert.equal(addFiveTo(-10), -5)
+    })
+  })
+})
