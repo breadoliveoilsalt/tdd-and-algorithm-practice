76b219a63aa185b50699cb2e5aa8a9a5af9a0339
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-01 15:43:50 -0400

    Draft insertions of chai assertions

--- test/test.js
+++ test/test.js
@@ -1,4 +1,9 @@
 
-var assert = require('assert')
+// var assert = require('assert')
+var chai = require('chai')
+var assert = require('chai').assert
+var expect = chai.expect
+var should = chai.should();
+
 
 // REMEMBER TO REQUIRE OTHER FILES, EG - let firstTest = require("./myFirstTests.js")
