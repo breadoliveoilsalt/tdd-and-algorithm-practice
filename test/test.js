a9b8f0c1b2850621c641ef2b89c75dc532096729
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-03 15:39:04 -0400

    Create separate test file for testAlgo as demo. Passes all tests.

--- test/test.js
+++ test/test.js
@@ -1,38 +1,38 @@
 
 // var assert = require('assert')
-var chai = require('chai')
-var assert = chai.assert
-var expect = chai.expect
-var should = chai.should()
+// var chai = require('chai')
+// var assert = chai.assert
+// var expect = chai.expect
+// var should = chai.should()
 
-var algos = require('../index')
+// var algos = require('../index')
 
-describe("Test", function(){
-
-  describe("Testing #addFiveTo() with assert", function(){
-    it("should return a number equal to five plus the argument passed to the function",
-    function(){
-      console.log(algos)
-      assert.equal(algos.addFiveTo(7), 12)
-      assert.equal(algos.addFiveTo(0), 5)
-      assert.equal(algos.addFiveTo(-10), -5)
-    })
-  })
-
-  describe("Testing #addFiveTo() with should", function() {
-    it("should return a number equal to five plus the argument passed to the function",
-    function(){
-      algos.addFiveTo(5).should.equal(10)
-    })
-  })
-
-  describe("Testing #addFiveTo() with expect", function() {
-    it("should return a number equal to five plus the argument passed to the function",
-    function(){
-      let result = algos.addFiveTo(17)
-      expect(result).to.equal(22)
-      // Note: this does not work: expect(addFiveTo(10)).to.equal(15))
-    })
-  })
-
-})
+// describe("Test", function(){
+//
+//   describe("Testing #addFiveTo() with assert", function(){
+//     it("should return a number equal to five plus the argument passed to the function",
+//     function(){
+//       console.log(algos)
+//       assert.equal(algos.addFiveTo(7), 12)
+//       assert.equal(algos.addFiveTo(0), 5)
+//       assert.equal(algos.addFiveTo(-10), -5)
+//     })
+//   })
+//
+//   describe("Testing #addFiveTo() with should", function() {
+//     it("should return a number equal to five plus the argument passed to the function",
+//     function(){
+//       algos.addFiveTo(5).should.equal(10)
+//     })
+//   })
+//
+//   describe("Testing #addFiveTo() with expect", function() {
+//     it("should return a number equal to five plus the argument passed to the function",
+//     function(){
+//       let result = algos.addFiveTo(17)
+//       expect(result).to.equal(22)
+//       // Note: this does not work: expect(addFiveTo(10)).to.equal(15))
+//     })
+//   })
+//
+// })
