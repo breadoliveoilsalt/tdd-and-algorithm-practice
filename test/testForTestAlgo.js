02ddf3ee4d240acb5257897eaf483ebd60e1b585
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-12 17:13:51 -0400

    In testForTestAlo, make changes so I can test commands at command line to test only a single file.
    Lessons are in sampleCommandsForTerminal and process.md

--- test/testForTestAlgo.js
+++ test/testForTestAlgo.js
@@ -1,22 +1,26 @@
 // Works but commented out.
 
-// var chai = require('chai')
-// var assert = chai.assert
-// var expect = chai.expect
-// var should = chai.should()
+var chai = require('chai')
+var assert = chai.assert
+var expect = chai.expect
+var should = chai.should()
 //
 // var addFiveTo = require('../algos/testAlgo').addFiveTo
 //
-// describe("Test", function(){
-//
-//   describe("Testing #addFiveTo() with assert", function(){
-//     it("should return a number equal to five plus the argument passed to the function",
-//     function(){
-//       assert.equal(addFiveTo(7), 12)
-//       assert.equal(addFiveTo(0), 5)
-//       assert.equal(addFiveTo(-10), -5)
-//     })
-//   })
+
+console.log("This is the testForTestAlgo file")
+
+describe("Test", function(){
+
+  describe("Testing #addFiveTo() with assert", function(){
+    it("should return a number equal to five plus the argument passed to the function",
+    function(){
+      assert.equal(addFiveTo(7), 12)
+      assert.equal(addFiveTo(0), 5)
+      assert.equal(addFiveTo(-10), -5)
+    })
+  })
+})
 //
 //   describe("Testing #addFiveTo() with should", function() {
 //     it("should return a number equal to five plus the argument passed to the function",
